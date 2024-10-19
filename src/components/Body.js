af3ff8e4ed3903d1eeeba035147cc5402b2cb8b1
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-10-19 18:01:36 +0000

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,8 @@ import Shimmer from "./Shimmer";
 const Body = () => {
 
     const [listOfRestaurants, setListOfRestaurants] = useState([])
+    const [filteredRestro, setFilteredRestro] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     useEffect(()=>{
         fetchData();
@@ -16,7 +18,7 @@ const Body = () => {
         console.log(json);
         const jsonData = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         setListOfRestaurants(jsonData);
-        
+        setFilteredRestro(jsonData);
     }
 
     // if(){
@@ -28,20 +30,38 @@ const Body = () => {
     return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className='body'>
             <div className="filter">
+
+                <div className="search">
+                    <input type="text" placeholder="Search Restaurant" className="search-box" value={searchText} onChange={(e)=>{
+                        setSearchText(e.target.value);
+                    }} />
+                    <button onClick={()=>{
+                         const trimmedSearchText = searchText.trim();  // Remove whitespace from both ends
+
+                         if (trimmedSearchText === "") {
+                           // If searchText is empty or only contains spaces, reset to the full list
+                           setFilteredRestro(listOfRestaurants);
+                         } else {
+                        const filteredRestros = listOfRestaurants.filter((item) => { return item.info?.name?.toLowerCase().includes(trimmedSearchText.toLowerCase());
+                        });
+                        setFilteredRestro(filteredRestros);
+                    }
+                                              
+                    }} >Search</button>
+                </div>
+
                 <button className="filter-btn"
                 onClick={() => {
-                    const filteredList = listOfRestaurants.filter((res)=>res.info.avgRating > 4);
+                    // console.log(res);
+                    const filteredList = listOfRestaurants.filter((res)=>res.info?.avgRating > 4);
                     // console.log(filteredList)
-                    setListOfRestaurants(filteredList);
+                    setFilteredRestro(filteredList);
                 }}
                 >Show Top Rated</button>
             </div>
-            <div className='search'>
-                Search Bar
-            </div>
             <div className='restro-container'>
                 {/*can use map here to send data in chunks*/}
-                <RestaurantCard resData = {listOfRestaurants}/>
+                <RestaurantCard resData = {filteredRestro}/>
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-import {useState} from "react";
 
 const Header = () => {
 
-    const [btn, setBtn] = useState("login");
+    const [btn, setBtn] = useState("Login"); 
 
     return(
         <div className='header'>
@@ -17,8 +17,9 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
-                    <button className="login" onClick>Login</button>
-                    {/* <div className=''></div> */}
+                    <button className="login" onClick={()=>{
+                        btn === "Login" ? setBtn("Logout") : setBtn("Login") 
+                    }}>{btn}</button>
                 </ul>
             </div>
         </div>

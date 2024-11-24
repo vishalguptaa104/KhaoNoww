1e8f871075f3b49e218cf42c073504e26aca2502
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-11-24 15:44:13 +0000

    Implemented HOF

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import RestaurantCard from "./RestaurantCard";
+import RestaurantCard, { withPromotedLabel } from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
@@ -6,58 +6,83 @@ import useRestaurantData from "../utils/useRestaurantData";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
+  const getStatus = useOnlineStatus();
+  const [searchText, setSearchText] = useState("");
+  const { listOfRestaurants, filteredRestro, setFilteredRestro } =
+    useRestaurantData();
 
-    const getStatus = useOnlineStatus();
-    const [searchText, setSearchText] = useState("");
-    const { listOfRestaurants, filteredRestro, setFilteredRestro } = useRestaurantData();
+  if (getStatus === false) {
+    return <h1>You are offline!</h1>;
+  }
 
-    if(getStatus === false){
-        return(
-            <h1>You are offline!</h1>
-        )
-    }
+  const PromotedLabel = withPromotedLabel(RestaurantCard);
 
-    return listOfRestaurants.length === 0 ? <Shimmer /> : (
-        <div className='body'>
-            <div className="flex justify-between">
-                <div className="m-4 p-4 rounded-lg">
-                    <input type="text" placeholder="Search Restaurant" className="border border-solid border-black" value={searchText} onChange={(e)=>{
-                        setSearchText(e.target.value);
-                    }} />
-                    <button 
-                        className="px-4 py-2 bg-green-100 m-4 rounded-lg"
-                        onClick={()=>{
-                         const trimmedSearchText = searchText.trim();  // Remove whitespace from both ends
-                         if (trimmedSearchText === "") {
-                           // If searchText is empty or only contains spaces, reset to the full list
-                           setFilteredRestro(listOfRestaurants);
-                         } else {
-                        const filteredRestros = listOfRestaurants.filter((item) => { return item.info?.name?.toLowerCase().includes(trimmedSearchText.toLowerCase());
-                        });
-                        setFilteredRestro(filteredRestros);
-                    }
-                                              
-                    }} >Search</button>
-                </div>
-
-                <button className="px-4 py-2 bg-green-100 m-12 rounded-lg"
-                onClick={() => {
-                    const filteredList = listOfRestaurants.filter((res)=>res.info?.avgRating > 4);
-                    setFilteredRestro(filteredList);
-                }}
-                >Show Top Rated</button>
-            </div>
-            
-            <div className='flex flex-wrap'>  
-                {filteredRestro.map((restaurant) => ( 
-                    <Link key={restaurant.info.id} 
-                    to={"/restaurants/"+restaurant.info.id}
-                    style={{textDecoration: 'none'}}>
-                        <RestaurantCard resData={restaurant?.info} /></Link>
-                ))}      
-            </div>
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="flex justify-between">
+        <div className="m-4 p-4 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search Restaurant"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              const trimmedSearchText = searchText.trim(); // Remove whitespace from both ends
+              if (trimmedSearchText === "") {
+                // If searchText is empty or only contains spaces, reset to the full list
+                setFilteredRestro(listOfRestaurants);
+              } else {
+                const filteredRestros = listOfRestaurants.filter((item) => {
+                  return item.info?.name
+                    ?.toLowerCase()
+                    .includes(trimmedSearchText.toLowerCase());
+                });
+                setFilteredRestro(filteredRestros);
+              }
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
+
+        <button
+          className="px-4 py-2 bg-green-100 m-12 rounded-lg"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info?.avgRating > 4
+            );
+            setFilteredRestro(filteredList);
+          }}
+        >
+          Show Top Rated
+        </button>
+      </div>
+
+      <div className="flex flex-wrap">
+        {filteredRestro.map((restaurant) => (
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
+            style={{ textDecoration: "none" }}
+          >
+            {restaurant?.info?.promoted ? (
+              <PromotedLabel resData={restaurant?.info} />
+            ) : (
+              <RestaurantCard resData={restaurant?.info} />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,28 +1,38 @@
 import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = ({ resData }) => {
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData;
+  const deliveryTime = sla ? sla.deliveryTime : "N/A"; // handle deliveryTime
 
-    // console.log(resData);
-    
-    const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } = resData;
-    const deliveryTime = sla ? sla.deliveryTime : 'N/A'; // handle deliveryTime
-
-    
-
-    return (
-                    <div className='m-4 p-4 w-[190px] h-[350px] rounded-lg bg-gray-100 hover:bg-gray-200'>
-                        <img 
-                            className='rounded-lg w-[160px] h-[160px]'
-                            alt="res-logo"
-                            src={CDN_URL + cloudinaryImageId}
-                        />
-                        <h3 className="font-bold py-1 text-lg truncate">{name}</h3>
-                        <h4 className="py-1">Ratings: {avgRating}</h4>
-                        <h4 className="font-bold py-1 text-lg truncate">{cuisines.join(', ')}</h4>
-                        <h4 className="py-1">Price : {costForTwo}</h4>
-                        <h5 className="py-1">ETA - {deliveryTime} mins</h5>
-                    </div>
-    );
+  return (
+    <div className="m-4 p-4 w-[190px] h-[350px] rounded-lg bg-gray-100 hover:bg-gray-200">
+      <img
+        className="rounded-lg w-[160px] h-[160px]"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-1 text-lg truncate">{name}</h3>
+      <h4 className="py-1">Ratings: {avgRating}</h4>
+      <h4 className="font-bold py-1 text-lg truncate">{cuisines.join(", ")}</h4>
+      <h4 className="py-1">Price : {costForTwo}</h4>
+      <h5 className="py-1">ETA - {deliveryTime} mins</h5>
+    </div>
+  );
 };
 
+//Implemented Higher Order Function to enhance the card with the promoted label
+export const withPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <Label className="absolute bg-black text-white m-2 p-2 rounded-lg">
+          Promoted
+        </Label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
+};
 
 export default RestaurantCard;

914caa1ef3df76f12b2ea350db6168aad519331b
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-11-25 16:33:40 +0000

    Lifted State UP!!! Child->Parent Component updates

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,37 +1,56 @@
 import useRestaurantMenu from "../utils/useRestaurantMenu.js";
 import Shimmer from "./Shimmer.js";
 import { useParams } from "react-router";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
-    
-    const resId = useParams();
-    const resInfo = useRestaurantMenu(resId);
+  const resId = useParams();
+  const resInfo = useRestaurantMenu(resId);
 
-    const {
-        name= "Default Restaurant Name",
-        cuisines = ["Unavailable"],
-        costForTwoMessage = "Price Not Available"
-    } = resInfo?.cards[2]?.card?.card?.info || {};
+  const dummy = "Dummy Data";
 
-    const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card || {};
-    console.log(itemCards)
+  const [showIndex, setShowIndex] = useState(null);
 
-    return (resInfo === null) ? <Shimmer /> : (
-        <>
-            <h1>{name}</h1>
-            <h3>{cuisines.join(", ")} - {costForTwoMessage}</h3>
-            <h3>Menu</h3>
-            <ul>
-               {itemCards.map((item) => (
-                <li key={item.card.info.id}>
-                    {item.card.info.name} - Rs.
-                    {item.card.info.price/100 || item.card.info.defaultPrice/100}
-                </li>
+  const {
+    name = "Default Restaurant Name",
+    cuisines = ["Unavailable"],
+    costForTwoMessage = "Price Not Available",
+  } = resInfo?.cards[2]?.card?.card?.info || {};
 
-            ))}
-            </ul>
-        </>
-    )
-}
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card || {};
+
+  const categories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
+  console.log(categories);
+
+  return resInfo === null ? (
+    <Shimmer />
+  ) : (
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      {/* categories accordions */}
+      {categories.map((category, index) => (
+        // controlled component
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index === showIndex ? null : index)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default RestaurantMenu;

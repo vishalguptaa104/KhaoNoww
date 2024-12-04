e4912129d972fac864f650fcaa3885ebb84df596
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-12-04 07:37:26 +0000

    Updates

---
+++ Grocery.2fa59245.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire492e;(0,e.register)("loR3J",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0");e("acw62");var o=()=>/*#__PURE__*/(0,i.jsx)("h1",{children:"This is a Grocery Page!!"})});
+//# sourceMappingURL=Grocery.2fa59245.js.map
